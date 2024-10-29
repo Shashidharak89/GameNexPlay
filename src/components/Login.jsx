19e828ba026820654f3b7c20fd7598f8abbfb168
Shashidharak89 <shashidharak334@gmail.com>
2024-10-29 20:57:40 +0530

    Initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+// src/components/Login.jsx
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <p>Please log in to access your account and personalized features.</p>
+      {/* Here you could add a login form */}
+    </div>
+  );
+};
+
+export default Login;
